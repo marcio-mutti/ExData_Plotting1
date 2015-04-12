421e4defcc1f46b51efcc33b4cb1d67b5c509d76
Marcio Mutti <marcio.mutti@gmail.com>
2015-04-11 23:45:46 -0300

    75%

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ data_hora<-paste(dados$Date, dados$Time, sep = " ")
 dados$Date<-strptime(data_hora, format = "%d/%m/%Y %H:%M:%S")
 dados$Time<-NULL
 d1<-strptime("2007-02-01", format = "%Y-%m-%d")
-d2<-strptime("2007-02-02", format = "%Y-%m-%d")
+d2<-strptime("2007-02-03", format = "%Y-%m-%d")
 dados<-dados[dados$Date>=d1 & dados$Date <=d2 , ]
 distribuicao<-hist(dados$Global_active_power, freq = TRUE, col = "red", plot = TRUE, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
--- plot3.R
+++ plot3.R
@@ -6,5 +6,10 @@ dados$Time<-NULL
 d1<-strptime("2007-02-01", format = "%Y-%m-%d")
 d2<-strptime("2007-02-03", format = "%Y-%m-%d")
 dados<-dados[dados$Date>=d1 & dados$Date <=d2 , ]
-plot(dados$Global_active_power, type = "l", xaxt="n", ylab = "Global Active Power (killowatts)", xlab = )
+plot(dados$Sub_metering_1, type = "n", xaxt="n", ylab = "Energy sub metering", xlab = "")
+#plot(, type = "l", col="red", xaxt="n", ylab = "", xlab = "", ylim=c(0,40))
+lines(dados$Sub_metering_1,col="black",xaxt="n", xlab="")
+lines(dados$Sub_metering_2,col="red",xaxt="n", xlab="")
+lines(dados$Sub_metering_3,col="blue",xaxt="n", xlab="")
 axis(1,at=c(1,1+1440,2881), labels = c("Thu","Fri","Sat"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1, 1, 1))
